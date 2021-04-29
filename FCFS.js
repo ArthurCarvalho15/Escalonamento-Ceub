aa6c436753198ae394f99c7393c3c5f22a52fa45
ArthurCarvalho15 <42048660+ArthurCarvalho15@users.noreply.github.com>
2021-04-28 21:31:15 -0300

    Update FCFS.js

--- FCFS.js
+++ FCFS.js
@@ -2,21 +2,27 @@ let processos = [];
 
 function wating_time(processos){
   let list1 = [];
-  for (i=0; i<processos.lenght; i++){
+  for (i=0; i<processos.length; i++){
     list1.push[0];
   }
   let tempo_servico = list1;
+  for (i=0; i<processos.length; i++){
+    tempo_servico[i] = 0;
+  }
 
   let list2 = [];
-  for (i=0; i < processos.lenght; i++){
+  for (i=0; i < processos.length; i++){
     list2.push[0];
   }
   let wt = list2;
+  for (i=0; i<processos.length; i++){
+    wt[i] = 0;
+  }
 
-  for (let i=1; i < processos.lenght; i++){
-    tempo_servico[i] = parseInt(tempo_servico[i-1] + processos[i-1][2]);
-    
-    wt[i] = parseInt(tempo_servico[i] - processos[i][1]);
+  for (let i=1; i < processos.length; i++){
+    tempo_servico[i] = parseInt(tempo_servico[i-1] + processos[i-1][1]);
+
+    wt[i] = parseInt(tempo_servico[i] - processos[i][0]);
     
     if (wt[i] < 0){
       wt[i] = 0;
@@ -27,14 +33,17 @@ function wating_time(processos){
 
 function turn_around_time(processos){
   let listTat = [];
-  for (i=0; i < processos.lenght; i++){
+  for (i=0; i < processos.length; i++){
     listTat.push[0];
   }
   let tat = listTat;
+  for (i=0; i<processos.length; i++){
+    tat[i] = 0;
+  }
 
   let wt = wating_time(processos);
-  for (let i=0; i < processos.lenght; i++){
-    tat[i] = parseInt(processos[i][2] + wt[i]);
+  for (let i=0; i < processos.length; i++){
+    tat[i] = parseInt(processos[i][1] + wt[i]);
   }
 
   return tat;
@@ -43,14 +52,14 @@ function turn_around_time(processos){
 function average_tat(processos){
   let tat = turn_around_time(processos);
   let sum = tat.reduce((a,b) => a+b, 0);
-  return (sum/processos.lenght);
+  return (sum/processos.length);
 }
 
 function average_wt(processos){
   let wt = wating_time(processos);
   let sum = wt.reduce((a,b) => a+b, 0);
 
-  return (sum/processos.lenght);
+  return (sum/processos.length);
 }
 
 
@@ -69,7 +78,7 @@ let avg_tat = average_tat(processos);
 
 console.log("| Process |\t| Burst Time |\t\t| Arrival Time |\t| Waiting Time |\t| Turn-Around Time |\t| Completion Time |\n\n")
 
-for (let proc=0; proc < processos.lenght; proc++){
+for (let proc=0; proc < processos.length; proc++){
   console.log(proc, "\t\t", processos[proc][1], "\t\t", processos[proc][0], "\t\t", wt[proc], "\t\t", tat[proc], "\t\t", parseInt(tat[proc] + processos[proc][0]));
 }
 
